de29c9f6a92387bcf8dd451468d75ada5ca0fd27
Magnus Isdal Kolbeinsen <Magnusik@stud.ntnu.no>
2021-11-14 20:42:23 +0100

    nothing

--- slam/EKFSLAM.py
+++ slam/EKFSLAM.py
@@ -306,7 +306,7 @@ class EKFSLAM:
         # TODO: You can set some assertions here to make sure that some of the structure in H is correct
         return H
 
-    def add_landmarks(
+def add_landmarks(
         self, eta: np.ndarray, P: np.ndarray, z: np.ndarray
     ) -> Tuple[np.ndarray, np.ndarray]:
         """Calculate new landmarks, their covariances and add them to the state.
@@ -326,18 +326,20 @@ class EKFSLAM:
             eta with new landmarks appended, and its covariance
         """
         # TODO replace this with your own code
-        etaadded_correct, Padded_correct = solution.EKFSLAM.EKFSLAM.add_landmarks(
-           self, eta, P, z)
+        etaadded, Padded = solution.EKFSLAM.EKFSLAM.add_landmarks(
+            self, eta, P, z)
+        return etaadded, Padded
 
         n = P.shape[0]
         assert z.ndim == 1, "SLAM.add_landmarks: z must be a 1d array"
 
         numLmk = z.shape[0] // 2
+
         lmnew = np.empty_like(z)
 
         Gx = np.empty((numLmk * 2, 3))
         Rall = np.zeros((numLmk * 2, numLmk * 2))
-        print(self.h(eta))
+
         I2 = np.eye(2)  # Preallocate, used for Gx
         # For transforming landmark position into world frame
         sensor_offset_world = rotmat2d(eta[2]) @ self.sensor_offset
@@ -349,66 +351,26 @@ class EKFSLAM:
             inds = slice(ind, ind + 2)
             zj = z[inds]
 
-            ## Measurement stuff
-            zrj = zj[0]  #radius
-            zphij= zj[1] #phi angle
-            print(zj)
-            #eta stuff
-            psi = eta[2] #psi/yaw angle
-
-            rot = rotmat2d(zphij+psi)  # TODO, rotmat in Gz
+            rot = None  # TODO, rotmat in Gz
             # TODO, calculate position of new landmark in world frame
-            x_z = zrj*np.cos(zphij)
-            y_z = zrj*np.sin(zphij)
+            lmnew[inds] = None
 
-            z_c = np.array([x_z,y_z])
+            Gx[inds, :2] = None  # TODO
+            Gx[inds, 2] = None  # TODO
 
-            lmnew[inds] =np.array([eta[0],eta[1]]) + rotmat2d(psi)@z_c+sensor_offset_world
-            ## Covariance robot state x
-            Gx[inds, :2] = I2  # TODO
-            vec = np.array([-np.sin(zphij+psi),np.cos(zphij+psi)])
-            Gx[inds, 2] =zrj*vec+sensor_offset_world_der
+            Gz = None  # TODO
 
-            # Covariance measured state z
-            IGz = I2 
-            IGz[1][1] = zrj
-            Gz = rot @ IGz
             # TODO, Gz * R * Gz^T, transform measurement covariance from polar to cartesian coordinates
-            J = np.array([[np.cos(zphij),-zrj*np.sin(zphij)],
-                        [np.sin(zphij),zrj*np.cos(zphij)]])
-            Gz_c = J@Gz@J.T
-    
-            Rall[inds, inds] = block_diag(Gz_c)
-            
-        #print("\n Rall",block_diag(Rall))
-        #print(lmnew)
+            Rall[inds, inds] = None
+
         assert len(lmnew) % 2 == 0, "SLAM.add_landmark: lmnew not even length"
-        #print("eta:",eta,"\nnew landmark",lmnew)
-        etaadded = np.block([eta,lmnew])  # TODO, append new landmarks to state vector
-        print("\n Shape etaadded:",np.shape(etaadded))
+        etaadded = None  # TODO, append new landmarks to state vector
         # TODO, block diagonal of P_new, see problem text in 1g) in graded assignment 3
-
-        print("shape P : ",np.shape(P), "\n shape Gx", np.shape(Gx))
-        print("shape Gz: ",np.shape(Gz))
-        print("\nn",n) 
-        # Padded
-        block2 = Gx*P[0,0]@Gx.T+block_diag(Rall)
-        Padded = block_diag(P,block2)
-
-        print("shape Padded : ",np.shape(Padded))
-        
-        Pcolon_x =P[:,0:3]
-        
-        print("Pcolon_x",Pcolon_x)
-
-        Padded[:n, n:] =Pcolon_x@Gx.T  # TODO, top right corner of P_new
-        print("\nshape:",np.shape(Padded[:n, n:]))
+        Padded = None
+        Padded[n:, :n] = None  # TODO, top right corner of P_new
         # TODO, transpose of above. Should yield the same as calcualion, but this enforces symmetry and should be cheaper
-        Padded[n:, :n] = Padded[:n, n:].T
-        # print("Correct etaadded",etaadded_correct)
-        # print("\nTest: etaadded",etaadded)
-        print("Test: Padded compare",Padded_correct-Padded)
-        print("\n Test: Padded",Padded)
+        Padded[:n, n:] = None
+
         assert (
             etaadded.shape * 2 == Padded.shape
         ), "EKFSLAM.add_landmarks: calculated eta and P has wrong shape"
@@ -421,7 +383,7 @@ class EKFSLAM:
 
         return etaadded, Padded
 
-    def associate(
+def associate(
         self, z: np.ndarray, zpred: np.ndarray, H: np.ndarray, S: np.ndarray,
     ):  # -> Tuple[*((np.ndarray,) * 5)]:
         """Associate landmarks and measurements, and extract correct matrices for these.
